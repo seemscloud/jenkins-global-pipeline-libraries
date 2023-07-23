dab6899961930594c4ed8135096eba5ee7a77a93
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-23 12:28:41 +0200

    dafs

---
+++ com/seemscloud/shared/RequestHandler.groovy
@@ -0,0 +1,13 @@
+package com.seemscloud.shared
+
+class Counter {
+    static int count = 0
+
+    static void increment() {
+        count++
+    }
+
+    static int getCount() {
+        return count
+    }
+}
