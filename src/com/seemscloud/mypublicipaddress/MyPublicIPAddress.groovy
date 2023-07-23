a430a45641da0d7069b4ae47d4d88810cbd9ae05
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-23 23:20:52 +0200

    asd

--- src/com/seemscloud/mypublicipaddress/MyPublicIPAddress.groovy
+++ src/com/seemscloud/mypublicipaddress/MyPublicIPAddress.groovy
@@ -4,7 +4,7 @@ import com.seemscloud.requests.HttpRequest
 
 class MyPublicIPAddress {
     static String getAddress() {
-        String result = HttpRequest.sendGetRequest("https://api.ipify.org?format=json")
+        def result = HttpRequest.sendGetRequest("https://api.ipify.org?format=json")
 
         println("Status Code ${result.responseCode}")
         if (result.responseCode == 200) {
