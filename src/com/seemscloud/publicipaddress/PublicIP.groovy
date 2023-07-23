d57240657c366a1c29e05fb2478a6b5292a1ad4c
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-24 01:37:16 +0200

    asd

--- src/com/seemscloud/publicipaddress/PublicIP.groovy
+++ src/com/seemscloud/publicipaddress/PublicIP.groovy
@@ -9,7 +9,7 @@ class PublicIP {
     static String getAddress(String format, String api) {
         String url = "https://${getApiType(api)}.ipify.org?format=${getFormatType(format)}"
 
-        LinkedHashMap<String, Object> result = HttpRequest.getRequestRaw(url)
+        LinkedHashMap<String, Object> result = HttpRequest.getRequest(url)
 
         if (result.responseCode == 200) {
             return result.response
--- src/com/seemscloud/requests/HttpRequest.groovy
+++ src/com/seemscloud/requests/HttpRequest.groovy
@@ -3,7 +3,7 @@ package com.seemscloud.requests
 import groovy.json.JsonSlurper
 
 class HttpRequest {
-    static LinkedHashMap<String, Object> getRequestRaw(String url) {
+    static LinkedHashMap<String, Object> getRequest(String url, Map<String, String> headers) {
         def connection = new URL(url).openConnection() as HttpURLConnection
         connection.requestMethod = 'GET'
 
