e134e307599a1b62d23d81acde83f86e77a0ca68
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-23 23:49:56 +0200

    asd

--- vars/getMyPublicIPAddress.groovy
+++ vars/getMyPublicIPAddress.groovy
@@ -1,5 +1,5 @@
 import com.seemscloud.mypublicipaddress.MyPublicIPAddress
 
 static def call() {
-    return MyPublicIPAddress.getFromIpify("json")
+    return MyPublicIPAddress.getFromIpify("jsonp")
 }
