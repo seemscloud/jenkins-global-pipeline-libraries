a0c703d55605aa38d3e5464b00af1cb5de55de9d
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-24 00:49:27 +0200

    asd

--- src/com/seemscloud/publicipaddress/PublicIP.groovy
+++ src/com/seemscloud/publicipaddress/PublicIP.groovy
@@ -8,6 +8,7 @@ class PublicIP {
 
     static String getAddress(String format, String api) {
         String url = "https://${getApiType(api)}.ipify.org?format=${getFormatType(format)}"
+        
         LinkedHashMap<String, Object> result = HttpRequest.sendGetRequest(url)
 
         if (result.responseCode == 200) {
