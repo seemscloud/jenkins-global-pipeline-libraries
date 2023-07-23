433ba244dcf9b9af641f5616bbb1e71fd50c7a48
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-07-24 00:42:47 +0200

    asd

--- src/com/seemscloud/mypublicipaddress/PublicIP.groovy
+++ src/com/seemscloud/mypublicipaddress/PublicIP.groovy
@@ -3,19 +3,33 @@ package com.seemscloud.mypublicipaddress
 import com.seemscloud.requests.HttpRequest
 
 class PublicIP {
-    static String getAddress(String format = null) {
-        def result = null
+    static final Set<String> apis = ["api", "api64"]
+    static final Set<String> formats = ["text", "json", "jsonp"]
 
-        if (format != null) {
-            result = HttpRequest.sendGetRequest("https://api.ipify.org?format=${format}")
-        } else {
-            result = HttpRequest.sendGetRequest("https://api.ipify.org")
-        }
+    static String getAddress(String format = "text", String type = "IPv4") {
+        String url = "https://${getApiType(type)}.ipify.org?format=${getFormatType(format)}"
+        LinkedHashMap<String, Object> result = HttpRequest.sendGetRequest(url)
 
         if (result.responseCode == 200) {
             return result.response
         } else {
-            throw new Exception("Received a non 200 response code: ${result.responseCode}")
+            throw new Exception("Received a non-200 response code: ${result.responseCode}")
+        }
+    }
+
+    static String getFormatType(String format) {
+        if (formats.contains(format)) {
+            return format
+        } else {
+            return "json"
+        }
+    }
+
+    static String getApiType(String type) {
+        if (apis.contains(type)) {
+            return type
+        } else {
+            return "api"
         }
     }
 }
